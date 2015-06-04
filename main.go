cfe266a66e516b5c909c004f9f90ec4c8aa3450e
Sebastian Brych <sebastian.brych@netguru.pl>
2015-06-04 20:35:42 +0200

    initial commit - basic irc bot

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/thoj/go-ircevent"
+	"strings"
+)
+
+var channel = "#1125"
+
+func main() {
+	con := irc.IRC("murgrabia", "murgrabia")
+	err := con.Connect("krakow.irc.pl:6667")
+
+	if err != nil {
+		fmt.Println("Connection failed")
+		return
+	}
+
+	con.AddCallback("001", func(e *irc.Event) {
+		con.Join(channel)
+	})
+
+	con.AddCallback("JOIN", func(e *irc.Event) {
+		con.Privmsg(channel, "Siema nygusy!")
+	})
+
+	con.AddCallback("PRIVMSG", func(e *irc.Event) {
+		if strings.Contains(e.Message(), "!quit") {
+			con.SendRaw("QUIT :Wypierdalam. Elo!")
+			con.Quit()
+		}
+	})
+
+	con.Loop()
+}

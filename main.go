8bc6ebdd1b42718a8caaddcd824d9f4f88443a52
Sebastian Brych <sb@nwscx.com>
2015-09-26 13:21:31 +0200

    gofmt the code

--- main.go
+++ main.go
@@ -3,31 +3,31 @@ package main
 import (
 	"fmt"
 	"github.com/thoj/go-ircevent"
-	"io/ioutil"
 	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"strings"
 )
 
 type Friendlist struct {
-  Handle  string
-  Host    string
-  Flag    string
+	Handle string
+	Host   string
+	Flag   string
 }
 
 type Config struct {
-  Nick    string
-  Host    string
-  Server  string
-  Port    string
-  Channel string
+	Nick    string
+	Host    string
+	Server  string
+	Port    string
+	Channel string
 }
 
 func main() {
 
-  config      := readConfig()
-  friendlist  := loadFriendlist()
-	con         := irc.IRC(config.Nick, config.Nick)
-  err         := con.Connect(config.Server + ":" + config.Port)
+	config := readConfig()
+	friendlist := loadFriendlist()
+	con := irc.IRC(config.Nick, config.Nick)
+	err := con.Connect(config.Server + ":" + config.Port)
 
 	if err != nil {
 		fmt.Println("Connection failed")
@@ -43,54 +43,54 @@ func main() {
 	})
 
 	con.AddCallback("PRIVMSG", func(e *irc.Event) {
-    msgParts := strings.Split(e.Arguments[1], " ")
+		msgParts := strings.Split(e.Arguments[1], " ")
 
-    if friendlist.Host == (e.User + "@" + e.Host) {
-      switch {
-      case msgParts[0] == "!quit":
-        if len(msgParts) > 1 {
-          con.SendRaw("QUIT :" + msgParts[1])
-        } else {
-          con.SendRaw("QUIT :I quit")
-        }
-        con.Quit()
+		if friendlist.Host == (e.User + "@" + e.Host) {
+			switch {
+			case msgParts[0] == "!quit":
+				if len(msgParts) > 1 {
+					con.SendRaw("QUIT :" + msgParts[1])
+				} else {
+					con.SendRaw("QUIT :I quit")
+				}
+				con.Quit()
 
-      case msgParts[0] == "!join":
-        con.Join(msgParts[1])
+			case msgParts[0] == "!join":
+				con.Join(msgParts[1])
 
-      default:
-        fmt.Println(e.Arguments)
-      }
-    }
+			default:
+				fmt.Println(e.Arguments)
+			}
+		}
 	})
 
 	con.Loop()
 }
 
-func readConfig() (Config) {
-  var config Config
-  configFile, err := ioutil.ReadFile("config")
+func readConfig() Config {
+	var config Config
+	configFile, err := ioutil.ReadFile("config")
 
-  if err != nil {
-    fmt.Println("Config could not be loaded:")
-    fmt.Println(err)
-  }
+	if err != nil {
+		fmt.Println("Config could not be loaded:")
+		fmt.Println(err)
+	}
 
-  err = yaml.Unmarshal(configFile, &config)
+	err = yaml.Unmarshal(configFile, &config)
 
-  return config
+	return config
 }
 
-func loadFriendlist() (Friendlist) {
-  var friendlist Friendlist
-  friendlistFile, err := ioutil.ReadFile("friendlist")
+func loadFriendlist() Friendlist {
+	var friendlist Friendlist
+	friendlistFile, err := ioutil.ReadFile("friendlist")
 
-  if err != nil {
-    fmt.Println("Friendlist could not be loaded")
-    fmt.Println(err)
-  }
+	if err != nil {
+		fmt.Println("Friendlist could not be loaded")
+		fmt.Println(err)
+	}
 
-  err = yaml.Unmarshal(friendlistFile, &friendlist)
+	err = yaml.Unmarshal(friendlistFile, &friendlist)
 
-  return friendlist
+	return friendlist
 }
